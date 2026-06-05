c32394f815f89a8bc8d7f4ffc15a40b302310d02
1268482 <wiilwin2729@gmail.com>
2026-06-05 12:02:59 +0530

    code formatted

--- prefix_sum_seq.cpp
+++ prefix_sum_seq.cpp
@@ -1,20 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 typedef long long ll;
+
 int main() {
     int n;
-    cout<<"the size of the vector:";cin>>n;
+    cout << "the size of the vector:";
+    cin >> n;
+
     vector<ll> vec(n);
-   cout<<"enter the vector:" ;
-   for(int i=0;i<n;++i){cin>>vec[i];}
-   vector<ll> p_sum(n,0);
-   for(int i=1;i<n;++i){
-    vec[i]+=vec[i-1];
-   }
-cout<<"the prefix sum vector:";
-for(int i=0;i<n;++i){
-    cout<<vec[i]<<" ";
-}
+    cout << "enter the vector:";
+    for (int i = 0; i < n; ++i) {
+        cin >> vec[i];
+    }
+
+    vector<ll> p_sum(n, 0);
+    for (int i = 1; i < n; ++i) {
+        vec[i] += vec[i - 1];
+    }
+
+    cout << "the prefix sum vector:";
+    for (int i = 0; i < n; ++i) {
+        cout << vec[i] << " ";
+    }
 
     return 0;
 }
